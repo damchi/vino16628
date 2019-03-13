d4ea45e6793e0075e60bf1f6f6f79b04c3830d4e
Damien Raffi <damien@Damiens-MacBook-Pro.local>
2019-03-13 16:52:37 -0400

    Ajout user

---
+++ modeles/Usager.class.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: damien
+ * Date: 13/03/2019
+ * Time: 09:58
+ */
+
+class Usager
+{
+
+}
---
+++ vues/ajoutUsager.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: damien
+ * Date: 13/03/2019
+ * Time: 10:28
+ */
