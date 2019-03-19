f7a7e74aa426190da9816d765173f8177c51747e
maxjr0 <maxjr0@gmail.com>
2019-03-19 02:54:29 -0400

    ajout menu_user

---
+++ vues/menu_user.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Max Germain
+ * Date: 2019-03-16
+ * Time: 03:39
+ */
+?>
+
+<header class="entete">
+    <div class="enteteImage">
+        <img src="images/bottle-50573_1280.jpg" alt="enteteImage">
+    </div>
+
+    <nav>
+        <ul class="topnav">
+            <li>
+                <a href="?requete=accueil">Mon cellier<span class="sr-only">(current)</span></a>
+            </li>
+            <li>
+                <a href="?requete=ajouterNouvelleBouteilleCellier">Ajouter une bouteille au cellier</a>
+            </li>
+        </ul>
+    </nav>
+</header>
+
