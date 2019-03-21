4b819bf8ef49e955274d4732ed7a6a6793e6fe0d
Yves Chouinard <yves.chouinard@gmail.com>
2019-03-21 13:52:43 -0400

    Marges au main pour grand format

--- vues/listeBouteilles.php
+++ vues/listeBouteilles.php
@@ -1,26 +1,21 @@
-<div class="options carte-pied">
-    <button><a href="index.php?requete=nouvelleBouteilleCellier&idCellier=<?= $data['idCellier'] ?>">Ajouter une bouteille</a></button>
+<!-- Bouton Ajouter une bouteille -->
+<div class="boutonHaut">
+    <button><a href="index.php?requete=nouvelleBouteilleCellier&idCellier=<?= $data['idCellier'] ?>"><i class="fas fa-plus"></i>  Ajouter une bouteille</a></button>
 </div>
 
 <div class="listeBouteille">    
-
     <?php
-//    foreach ($data['listeBouteilles'] as $cle => $bouteille) {
+    foreach ($data['listeBouteilles'] as $cle => $bouteille) {
     ?>
-    <?php foreach ($data['listeBouteilles'] as $cle => $bouteille) { ?>
 
     <!-- Carte -->
     <div class="carte bouteille" data-id="<?php echo $bouteille['id_bouteille'] ?>">
-
         <!-- Titre -->
         <div class="carte-titre">
             <h4><?php echo $bouteille['nom'] ?></h4>
         </div>
 
         <div class="carte-description">
-            <!-- Icone -->
-<!--            <h5><i class="fas fa-glass-cheers"></i> Vins</h5>-->
-
             <!-- Texte -->
             <p class="carte-texte">Quantité : <span class="quantite"><?php echo $bouteille['quantite'] ?></span></p>
             <p class="carte-texte">Pays : <?php echo $bouteille['pays'] ?></p>
@@ -32,26 +27,18 @@
         <!-- Carte image -->
         <div class="carte-image">
             <img src="https:<?php echo $bouteille['url_img'] ?>" alt="Image de la bouteille">
-            <a>
-                <div class="mask rgba-white-slight"></div>
-            </a>
         </div>
 
         <!-- Bouton -->
-        <div class="options carte-pied">
-            <button class='btnAjouter'>Ajouter</button>
-            <button class='btnBoire'>Boire</button>
-            <button class='btnModifier'><a href="index.php?requete=modifierBouteille&idBouteille=<?= $bouteille['id_bouteille'] ?>">Modifier</a></button>
-            <button class='btnSupprimer'>Supprimer</button>
+        <div class="carte-pied">
+            <button class='btnAjouter'><i class="fas fa-plus"></i></button>
+            <button class='btnBoire'><i class="fas fa-minus"></i></button>
+            <button class='btnModifier'><a href="index.php?requete=modifierBouteille&idBouteille=<?= $bouteille['id_bouteille'] ?>"><i class="fas fa-edit"></i></a></button>
+            <button class='btnSupprimer'><i class="fas fa-trash-alt"></i></button>
         </div>
     </div>
-    <!-- Carte -->
     
     <?php
     }
     ?>
-
-</div>
-
-<!--    --><?php //} ?>
 </div>
