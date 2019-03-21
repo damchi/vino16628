5fde760d90a18e66754e5703c6a6055e182e1562
maxjr0 <maxjr0@gmail.com>
2019-03-21 12:19:47 -0400

    maj avant pull

--- vues/login.php
+++ vues/login.php
@@ -7,40 +7,39 @@
  */
 
 ?>
+<div class="formlogin">
+    <form class="form" method="POST" action="index.php?requete=logedin">
+        <div class="group">
+            <label class="icon_form"><i class="fas fa-user"></i></label>
+            <input type="text" id="identifiantLogin" name="identifiant" placeholder="Nom utilisateur ou email">
+        </div>
+        <div class="group">
+            <label class="icon_form"><i class="fas fa-lock"></i></label>
+            <input type="password" id="passwordLogin" name="password" placeholder="mot de passe">
+        </div>
+        <div class="group">
+            <input type="submit" class="btn" id="btnLogin" name="btnLogin" value="Se connecter">
+        </div>
+        <!--    <div class="" vertical layout>-->
+    <!--        <div >-->
+    <!--            <p>Nom utilisateur ou email<input type="text" name="identifiant"></p>-->
+    <!--            <p>Mot de passe : <input type="password" name="mdp"></p>-->
+    <!--        </div>-->
+    <!--        <button name="btnLogin">Se connecter</button>-->
+    <!--    </div>-->
+    <!--</div>-->
+        <div id="errorLogin">
+            <?php
+            if (isset($dataMessage)){
+                echo "<p id='message'>". $dataMessage."</p>";
+            }
+            ?>
 
-<form method="POST" action="index.php?requete=logedin">
-    <div class="group">
-        <label class="icon_form"><i class="fas fa-user"></i></label>
-        <input type="text" id="identifiantLogin" name="identifiant" placeholder="Nom utilisateur ou email">
+        </div>
+    </form>
+
+    <div>
+    <a href="index.php?requete=inscription">Pas encore inscrit ? </a>
     </div>
-    <div class="group">
-        <label class="icon_form"><i class="fas fa-lock"></i></label>
-        <input type="password" id="passwordLogin" name="password" placeholder="mot de passe">
-    </div>
-    <div class="group">
-        <input type="submit" class="btn" id="btnLogin" name="btnLogin" value="Se connecter">
-    </div>
-    <!--    <div class="" vertical layout>-->
-<!--        <div >-->
-<!--            <p>Nom utilisateur ou email<input type="text" name="identifiant"></p>-->
-<!--            <p>Mot de passe : <input type="password" name="mdp"></p>-->
-<!--        </div>-->
-<!--        <button name="btnLogin">Se connecter</button>-->
-<!--    </div>-->
-<!--</div>-->
-    <div id="errorLogin">
-        <?php
-        if (isset($dataMessage)){
-            echo "<p id='message'>". $dataMessage."</p>";
-        }
-        ?>
 
-    </div>
-</form>
-
-
-
-<div>
-<a href="index.php?requete=inscription">Pas encore inscrit ? </a>
 </div>
-
