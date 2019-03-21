9834e28e95d2a289f5ad567f67bdb43cc3f9c085
unknown <juliengranero@live.ca>
2019-03-21 16:44:45 -0400

    changement du CSS et HTML du header

--- vues/menu_user.php
+++ vues/menu_user.php
@@ -6,41 +6,39 @@
  * Time: 03:39
  */
 ?>
-
 <header class="entete">
     <div class="enteteImage">
         <img src="images/bottle-50573_1280.jpg" alt="enteteImage">
     </div>
-    <nav>
-
-        <ul class="topnav">
-
-
+    <nav class="topnav" id="topnav">
     <?php
-     if( !isset($_SESSION['user_pseudo'])){
+    if(!isset($_SESSION['user_pseudo'])){
     ?>
-            <li>
-                <a href="?requete=login">Acceuil<span class="sr-only">(current)</span></a>
-            </li>
-            <?php
-            }
-            else{
+        <a href="?requete=login">Acceuil<span class="sr-only">(current)</span></a>
+    <?php
+    }
+    else{
 //         var_dump($_SESSION['user_pseudo']);
-         ?>
-         <li>
-             <a href="?requete=accueil">Mes celliers<span class="sr-only">(current)</span></a>
-         </li>
+    ?>
+        <a href="?requete=accueil">Mes celliers<span class="sr-only">(current)</span></a>
 <!--                 <li>-->
 <!--                     <a href="?requete=ajouterNouvelleBouteilleCellier">Ajouter une bouteille au cellier</a>-->
 <!--                 </li>-->
-         <li>
-             <a href="?requete=logout">Se deconnecter</a>
-         </li>
-         <?php
-     }
+        <a href="?requete=logout">Se deconnecter</a>
+    <?php
+    }
     ?>
-        </ul>
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+        <i class="fas fa-bars"></i>
     </nav>
-
 </header>
-
+<script type="text/javascript">
+    function myFunction() {
+  var x = document.getElementById("topnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+</script>
