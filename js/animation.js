0fed76c2cbd1fb0e6409a4bf86440e315c470e1f
Granero <e1695559@cmaisonneuve.qc.ca>
2019-03-22 11:08:13 -0400

    ajout image bouteille_vin.png

---
+++ js/animation.js
@@ -0,0 +1,8 @@
+function animationLogin() {
+  document.querySelectorAll(".mouvante").addEventListener("click", function () {
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+  	}
+})}
