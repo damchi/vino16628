55ac106b7c328a98b29943193c2d4cacdd7913bd
Granero <e1695559@cmaisonneuve.qc.ca>
2019-03-25 11:26:27 -0400

    creation du fichier animation

--- js/animation.js
+++ js/animation.js
@@ -1,8 +1,21 @@
+window.addEventListener("load", function () {
+		animationLogin();
+})
+
 function animationLogin() {
-  document.querySelectorAll(".mouvante").addEventListener("click", function () {
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-  	}
-})}
+  document.querySelectorAll(".mouvante").forEach(mouvante => mouvante.addEventListener("click", function() {
+
+  	var elementAMontrer = this.nextElementSibling;
+  	console.log(elementAMontrer);
+  	if (this.id == 1) {
+  	  var elementACacher = this.parentNode.childNodes[9];
+  	  console.log(elementACacher);
+	} 
+	else {
+	  var elementACacher = this.previousElementSibling;
+	  console.log(elementACacher);
+	}
+	elementAMontrer.style.maxHeight = elementAMontrer.scrollHeight + "px";
+	elementACacher.style.maxHeight = 0;
+  }
+))}
