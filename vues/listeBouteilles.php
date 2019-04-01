1bef5783bd55c4515ec68fa9f9d5d05444f2644e
maxjr0 <maxjr0@gmail.com>
2019-04-01 09:08:48 -0400

    changement mobile listeBouteille.php et css correspondant

--- vues/listeBouteilles.php
+++ vues/listeBouteilles.php
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-<div class="hautDePage">
-
-    <!-- Recherche Bouteille-->
-    <div>
-        <div id="recherche_bouteille_cellier">
-            <label class="icon_form"><i class="fas fa-search"></i></label>
-            <input type="text" name="rechercheInfo" placeholder="Recherche">
-        </div>
-        <ul class="listeAutoComplete"></ul>
-    </div>
-=======
 
 
 <div class="hautDePage">
@@ -19,13 +7,11 @@
     </div>
 
     <!-- Recherche Bouteille-->
-
     <div id="recherche_bouteille_cellier" class="group">
         <label class="icon_form"><i class="fas fa-search"></i></label>
         <input type="text" name="rechercheInfo" placeholder="Recherche">
     </div>
   
->>>>>>> 21db48298d0fc9eb14eea0a6d9a028f08949725e
     <!--Bouton choisir affichage-->
     <div class="afficheListe">
         <button class="afficherListeBouteille"><i class="fas fa-list"></i></button>
@@ -34,14 +20,12 @@
 </div>
 
 <div class="recherche">
-<<<<<<< HEAD
-=======
 
     <ul class="listeAutoComplete"></ul>
->>>>>>> 21db48298d0fc9eb14eea0a6d9a028f08949725e
     <input type="hidden" name="idCellier" value="<?= $_SESSION['idCellier']?>">
-    <button>Filtrer par</button>
-    <div class="filtreRecherche">
+    <button>Filtrer par &nbsp; <i class="fas fa-angle-down"></i> </button>
+    
+    <div id="filtreRecherche" class="mc">
         <select name="pays">
             <option value="">Pays</option>
             <?php
@@ -96,58 +80,62 @@
     <!-- Carte -->
     <div class="carte bouteille" data-id="<?php echo $bouteille['id_bouteille'] ?>">
         <!-- Titre -->
-        <div class="carte-titre">
-            <h4><?php echo $bouteille['nom'] ?></h4>
+        <div class="carte-entete">
+            <div class="carte-titre">
+                <?php echo $bouteille['nom'] ?>
+            </div>
         </div>
 
-        <div class="carte-information">
-            <!-- Texte -->
-            <div class="carte-quantite">Quantité : <span class="quantite"><?php echo $bouteille['quantite'] ?></span></div>
-            <div class="carte-pays">Pays : <?php echo $bouteille['pays'] ?></div>
-            <div class="carte-type">Type : <?php echo $bouteille['type'] ?></div>
-            <div class="carte-millesime">Millesime : <?php echo $bouteille['millesime'] ?></div>
-            <?php
-            if ($bouteille['code_saq'] != null ) {
-                ; ?>
-                <div class="carte-code_saq">Code saq : <a
-                            href="<?php echo $bouteille['url_saq'] ?>"><?php echo $bouteille['code_saq'] ?></a></div>
-                <?php
-            }
-            ?>
+        <div class="carte-description">
+            <div class="carte-information">
+                <!-- Texte -->
+                <div class="carte-quantite">Quantité : <span class="quantite"><?php echo $bouteille['quantite'] ?></span></div>
+                <div class="carte-pays">Pays : <?php echo $bouteille['pays'] ?></div>
+                <div class="carte-type">Type : <?php echo $bouteille['type'] ?></div>
+                <div class="carte-millesime">Millesime : <?php echo $bouteille['millesime'] ?></div>
+		        <?php
+		        if ($bouteille['code_saq'] != null ) {
+			        ; ?>
+                    <div class="carte-code_saq">Code saq : <a
+                                href="<?php echo $bouteille['url_saq'] ?>"><?php echo $bouteille['code_saq'] ?></a></div>
+			        <?php
+		        }
+		        ?>
 
-<!--            <!-- Your share button code -->
-<!--            <div class="fb-share-button"-->
-<!--                 data-href="https://www.your-domain.com/your-page.html"-->
-<!--                 data-layout="button_count">-->
-<!--            </div>-->
+                <!-- Your share button code -->
+                <!--<div class="fb-share-button"
+					 data-href="https://www.your-domain.com/your-page.html"
+					 data-layout="button_count">
+				</div>-->
 
-        </div>
+            </div>
 
-        <div class="carte-information_2">
-            <!-- Texte -->
-            <div class="carte-format"> Format : <?php echo $bouteille['format'] ?></span></div>
-            <div class="carte-pays">Date achat : <?php echo $bouteille['date_achat'] ?></div>
-            <div class="carte-type">garde jusqu'à : <?php echo $bouteille['garde_jusqua'] ?></div>
-            <div class="carte-millesime">Notes : <?php echo $bouteille['notes'] ?></div>
-        </div>
+            <div class="carte-information_2">
+                    <!-- Texte -->
+                <div class="carte-format"> Format : <?php echo $bouteille['format'] ?></span></div>
+                <div class="carte-pays">Date achat : <?php echo $bouteille['date_achat'] ?></div>
+                <div class="carte-type">garde jusqu'à : <?php echo $bouteille['garde_jusqua'] ?></div>
+                <div class="carte-millesime">Notes : <?php echo $bouteille['notes'] ?></div>
+            </div>
 
-        <!-- Carte image -->
-        <div class="carte-image">
-            <?php
+                <!-- Carte image -->
+            <div class="carte-image">
+               <?php
+                if ($bouteille['url_img'] == null){
+                    ?>
+                    <img src="./images/bouteille_vin.png" alt="Image de la bouteille">
 
-            if ($bouteille['url_img'] == null){
+                    <?php
+                }
+                else{
+                    ?>
+                    <img src="https:<?php echo $bouteille['url_img'] ?>" alt="Image de la bouteille">
+
+                    <?php
+                }
                 ?>
-                <img src="./images/bouteille_vin.png" alt="Image de la bouteille">
+            </div>
 
-                <?php
-            }
-            else{
-                ?>
-                <img src="https:<?php echo $bouteille['url_img'] ?>" alt="Image de la bouteille">
-
-                <?php
-            }
-            ?>
         </div>
 
         <!-- Bouton -->
