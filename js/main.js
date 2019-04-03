af2870926d19690a2172327b7703c80cf5b95a1d
Damien Raffi <damien@Damiens-MacBook-Pro.local>
2019-04-03 11:14:47 -0400

    correction affichage filtre

--- js/main.js
+++ js/main.js
@@ -13,14 +13,18 @@ console.log(BaseURL);
 
 window.addEventListener('load', () => {
     let burger = document.querySelector('.burger');
-
+    var x = document.getElementById("topnav");
     burger.addEventListener('click', () => {
-          var x = document.getElementById("topnav");
-
-          if (x.className === "topnav") {
-              x.className += " responsive";
+        console.log('e');
+        console.log(x)
+        console.log(x.className);
+          if (x.className == "topnav colNav") {
+            x.classList.remove('colNav');
+            x.classList.add("responsive");
           } else {
-                x.className = "topnav";
+            console.log('eeeeee');
+               x.classList.add('colNav');
+               x.classList.remove('responsive');
           }
     });    
 });
