c0a7ce55346fb9104b1658face736352d9966685
Damien Raffi <damien@Damiens-MacBook-Pro.local>
2019-04-05 11:37:26 -0400

    comment cellier

--- modeles/Cellier.class.php
+++ modeles/Cellier.class.php
@@ -86,18 +86,17 @@ class Cellier extends Modele
             $filetype = $_FILES["image"]["type"];
             $filesize = $_FILES["image"]["size"];
 
-            // Verify file extension
+            // Verifie extension
             $ext = pathinfo($filename, PATHINFO_EXTENSION);
             if(!array_key_exists($ext, $allowed)) die("Error: Please select a valid file format.");
 
-            // Verify file size - 5MB maximum
+            //taille max 5MB
             $maxsize = 5 * 1024 * 1024;
             if($filesize > $maxsize) die("Error: File size is larger than the allowed limit.");
 
-            // Verify MYME type of the file
+
             if(in_array($filetype, $allowed)){
-
-
+                // cree une string unique par image
                 $key = '';
                 $keys = array_merge(range(0, 9), range('a', 'z'));
 
@@ -107,21 +106,13 @@ class Cellier extends Modele
                 $filename = $key.$filename;
                 $image = $this->_db->escape_string($filename);
 
-
-                // Check whether file exists before uploading it
-//                if(file_exists("./images/" . $filename)){
-//                    echo $filename . " is already exists.";
-//                } else{
                 move_uploaded_file($_FILES["image"]["tmp_name"], "./images/" .$filename);
-//                    echo "Your file was uploaded successfully.";
-//                }
+
             } else{
                 echo "Error: There was a problem uploading your file. Please try again.";
             }
         }
-//        else{
-//            echo "Error: " . $_FILES["image"]["error"];
-//        }
+
 
 
 
@@ -133,8 +124,7 @@ class Cellier extends Modele
             $sql = " INSERT INTO " .self::TABLE."(nom,id_usager_cellier) VALUES ('".$nom."',".$id.")";
         }
 
-//        var_dump($sql);
-//        die();
+
         $this->_db->query($sql);
 
         $id = $this->_db->insert_id;
